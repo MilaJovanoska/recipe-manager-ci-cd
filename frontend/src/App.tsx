961321ca8e7f7e3d3f153c33b8cc027e30bf219b
Mila Jovanoska <mila.jovanoska25@gmail.com>
2026-06-22 10:53:03 +0200

    Add recipe management page and dialogs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import RecipesProvider from './providers/recipesProvider.tsx';
 import SnackbarProvider from './providers/snackbarProvider.tsx';
 import Layout from './ui/components/layout/Layout/Layout.tsx';
 import HomePage from './ui/pages/home/HomePage/HomePage.tsx';
+import RecipesPage from './ui/pages/recipe/RecipesPage/RecipesPage.tsx';
 
 const App = () => {
     return (
@@ -15,6 +16,7 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<Layout />}>
                             <Route index element={<HomePage />} />
+                            <Route path="recipes" element={<RecipesPage />} />
                             <Route path="*" element={<Navigate to="/" replace />} />
                         </Route>
                     </Routes>
