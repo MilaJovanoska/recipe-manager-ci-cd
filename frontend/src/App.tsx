2bc907c209ad1143312564dec73ce3943e39fc7f
Mila Jovanoska <mila.jovanoska25@gmail.com>
2026-06-22 10:57:34 +0200

    Add recipe details page and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import RecipesProvider from './providers/recipesProvider.tsx';
 import SnackbarProvider from './providers/snackbarProvider.tsx';
 import Layout from './ui/components/layout/Layout/Layout.tsx';
 import HomePage from './ui/pages/home/HomePage/HomePage.tsx';
+import RecipeDetailsPage from './ui/pages/recipe/RecipeDetailsPage/RecipeDetailsPage.tsx';
 import RecipesPage from './ui/pages/recipe/RecipesPage/RecipesPage.tsx';
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
                         <Route path="/" element={<Layout />}>
                             <Route index element={<HomePage />} />
                             <Route path="recipes" element={<RecipesPage />} />
+                            <Route path="recipes/:id" element={<RecipeDetailsPage />} />
                             <Route path="*" element={<Navigate to="/" replace />} />
                         </Route>
                     </Routes>
