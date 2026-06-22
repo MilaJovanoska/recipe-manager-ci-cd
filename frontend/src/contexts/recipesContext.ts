d929ed39432576f751de6671da96ca344e114e97
Mila Jovanoska <mila.jovanoska25@gmail.com>
2026-06-22 09:33:03 +0200

    Add frontend state management layer

---
+++ frontend/src/contexts/recipesContext.ts
@@ -0,0 +1,18 @@
+import { createContext } from 'react';
+import type {
+    CreateOrUpdateRecipeRequest,
+    RecipeResponse,
+} from '../api/types/recipe.ts';
+
+export interface RecipesContextType {
+    recipes: RecipeResponse[];
+    loading: boolean;
+    fetchRecipes: () => Promise<void>;
+    onAdd: (recipe: CreateOrUpdateRecipeRequest) => Promise<void>;
+    onEdit: (id: number, recipe: CreateOrUpdateRecipeRequest) => Promise<void>;
+    onDelete: (id: number) => Promise<void>;
+}
+
+const RecipesContext = createContext<RecipesContextType | undefined>(undefined);
+
+export default RecipesContext;
